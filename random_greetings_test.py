dfcdeae9b93b1d0e45aa8d9976220c9cc92c7dba
João Dartora <joao_dartora@sicredi.com.br>
2021-01-18 01:28:37 -0300

    test: adicionando primeiros testes unitarios

--- random_greetings_test.py
+++ random_greetings_test.py
@@ -1,9 +1,26 @@
 import unittest
 import random_greetings
 
-class MyTest(unittest.TestCase):
-    def get_random_greeting_with_success(self):
-        self.assertEqual(random_greetings.get_random_greeting(), "Boa noite meu Abacaxicultor")
+class RandomGreetings(unittest.TestCase):
+    
+    # abrir_csv
+    
+    # buscar_termos_validos
+
+    # definir_saudacao
+    def test_definir_saudacao_madrugada_com_sucesso(self):
+        self.assertEquals(random_greetings.definir_saudacao(3), "Boa madrugada")
+
+    def test_definir_saudacao_dia_com_sucesso(self):
+        self.assertEquals(random_greetings.definir_saudacao(7), "Bom dia")
+
+    def test_definir_saudacao_tarde_com_sucesso(self):
+        self.assertEquals(random_greetings.definir_saudacao(13), "Boa tarde")
+        
+    def test_definir_saudacao_noite_com_sucesso(self):
+        self.assertEquals(random_greetings.definir_saudacao(19), "Boa noite")
+
+    # montar_response
 
 if __name__ == '__main__':
     unittest.main()
