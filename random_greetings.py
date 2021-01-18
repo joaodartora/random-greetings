194cde02d783f29f58786adc378049b6f281a090
João Dartora <joao_dartora@sicredi.com.br>
2021-01-18 01:28:25 -0300

    refactor: parametrizando metodos para facilitar teste

--- random_greetings.py
+++ random_greetings.py
@@ -8,34 +8,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
-def abrir_csv():
-    nome_arquivo_cbo = 'cbo/lista.csv'
-    arquivo_csv = open(nome_arquivo_cbo, newline='')
+def abrir_csv(nome_arquivo):
+    arquivo_csv = open(nome_arquivo, newline='')
     return csv.reader(arquivo_csv, delimiter=',')
 
-def buscar_termos_validos():
-    arquivo_csv = abrir_csv()
+def buscar_termos_validos(arquivo_csv):
     termos = []
     for linha in arquivo_csv:
         if (len(linha) == 3) and (linha[2] != "Família"):
             termos.append(linha[1])
     return termos
 
-def definir_saudacao():
-    hora_atual = datetime.datetime.now().hour
+def definir_saudacao(hora_atual):
     if (hora_atual >= 0 and hora_atual < 6): return "Boa madrugada"
     elif (hora_atual >= 6 and hora_atual < 12): return "Bom dia"
     elif (hora_atual >= 12 and hora_atual < 18): return "Boa tarde"
     else: return "Boa noite"
 
-termos = buscar_termos_validos()
-
-@app.route("/random-greeting")
-def buscar_saudacao_aleatoria():
-    numero_randomico = random.randint(1, len(termos))
-    cumprimento = definir_saudacao() + ", meu " + termos[numero_randomico]
+def montar_response(cumprimento):
     response = {"cumprimento": cumprimento}
     return json.dumps(response, ensure_ascii=False).encode('utf8')
+
+arquivo_csv = abrir_csv("cbo/lista.csv")
+termos = buscar_termos_validos(arquivo_csv)
+
+@app.route("/random-greeting")
+def buscar_saudacao_aleatoria():    
+    numero_randomico = random.randint(1, len(termos))
+    hora_atual = datetime.datetime.now().hour
+    cumprimento = definir_saudacao(hora_atual) + ", meu " + termos[numero_randomico]
+    return montar_response(cumprimento)
 
 if __name__=='__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
